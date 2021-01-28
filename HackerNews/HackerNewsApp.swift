aa94df862c198d5b14d1d0532d5a9d4a8ac87b4a
VijayaMadhavi <vijayamadhavi@hotmail.com>
2021-01-28 17:44:13 +0530

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Vijaya Madhavi on 27/01/21.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
